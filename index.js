2c42dfb0d9eb26dff0dd66a439bd27c007ecbf61
ldv432 <ldv432@gmail.com>
2024-01-02 20:23:59 -0700

    made a palidrome thing

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+for (let i = 0; i < word.length/2; i++){
+  const j = word.length - 1 - i;
+  const startChar = word[i];
+  const endChar = word[j];
+  if (startChar !== endChar);
+  return false
+}
+return true
 }
 
 /* 
   Add your pseudocode here
+  -We're trying to create a function that takes in a word as an argument
+  -We want our function to iterate over our word from left to right and from right to left
+  -If those two iterations are the same, we want our function to return true
 */
 
 /*
   Add written explanation of your solution here
+We created a for loop to interate from the beginning to halfway through a word,
+we then created a new variable j to start from the end of a word to work backwards to the middle.
+we then created variables startChar/endChar to track each letter as we pass through the loop.
+we then made an if statement that said if they're not exactly equal, return false otherwise return true!
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +30,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("goog"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
